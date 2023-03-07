8e74d01d34a8e7f0b6350a59dbb751a1e74ac43c
Alexander Hunter <alexander.hunter-4@student.manchester.ac.uk>
2023-03-07 16:17:14 +0000

    Add ChatManager so that conversations are held for more than just one message

--- bot.py
+++ bot.py
@@ -3,6 +3,64 @@ import discord
 import json
 import openai
 import threading
+import time
+
+
+class ChatManager:
+    def __init__(self):
+        self.chats = []
+
+    def get_chat_response(self, msg: discord.message.Message, prompt: str) -> str:
+        chat = self.find_chat(msg)
+
+        if chat is None:
+            self.add_chat(msg)
+            chat = self.find_chat(msg)
+
+        chat["prompts"].append(prompt)
+        try:
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {"role": "user", "content": "\n".join(chat["prompts"])},
+                ]
+            )
+
+            # return the response from chatgpt
+            return response["choices"][0]["message"]["content"]
+        except Exception as e:
+            print(e)
+            return "Something went wrong. Please try again later."
+
+    def add_chat(self, chat: discord.message.Message) -> None:
+        return self.chats.append({
+            "expires": self.timeout,
+            "server": chat.guild.id,
+            "channel": chat.channel.id,
+            "prompts": []
+        })
+
+    def find_chat(self, msg: discord.message.Message) -> dict:
+        self.remove_expired_chats()
+
+        for chat in self.chats:
+            if chat["server"] == msg.guild.id and chat["channel"] == msg.channel.id:
+                return chat
+
+        return None
+
+    def remove_expired_chats(self) -> None:
+        for chat in self.chats:
+            if self.is_timed_out(chat):
+                self.chats.remove(chat)
+                return None # stop because we found the chat to remove
+
+    def is_timed_out(self, chat: dict) -> bool:
+        return time.time() > chat["expires"]
+
+    @property
+    def timeout(self) -> int:
+        return time.time() + 86400
 
 
 class Bot(discord.Client):
@@ -15,6 +73,9 @@ class Bot(discord.Client):
         # event loop so that we can get multiple responses from the GPT-3 API at the same time
         self.loop = asyncio.new_event_loop()
         asyncio.set_event_loop(self.loop)
+
+        # set up the ChatManager to handle chat responses
+        self.chat_manager = ChatManager()
 
         super().__init__(*args, **kwargs)
 
@@ -37,13 +98,13 @@ class Bot(discord.Client):
         await message.channel.send(response)
 
     def handle_message(self, message: discord.message.Message):
-        response = self.get_response(message.content)
+        response = self.get_response(message)
 
         if response is not None:
             self.loop.create_task(self.send_message(message, response))
 
-    def get_response(self, msg: str) -> str:
-        msg = msg.lower()
+    def get_response(self, message) -> str:
+        msg = message.content.lower()
 
         if msg == "!ping":
             return "pong"
@@ -61,22 +122,14 @@ class Bot(discord.Client):
                 return "Invalid prompt: to give a prompt, type `!gpt <prompt>`"
 
             # return prompt
-            return self.get_chat_gpt_response(prompt)
+            return self.get_chat_gpt_response(message, prompt)
 
         else:
             return None
 
-    def get_chat_gpt_response(self, prompt: str) -> str:
+    def get_chat_gpt_response(self, message, prompt: str) -> str:
         try:
-            response = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                messages=[
-                    {"role": "user", "content": prompt},
-                ]
-            )
-
-            # return the response from chatgpt
-            return response["choices"][0]["message"]["content"]
+            return self.chat_manager.get_chat_response(message, prompt)
         except Exception as e:
             print(e)
             return "Something went wrong. Please try again later."
